be1578af2738f0b0ee14c3bd8fd15f36c8160073
MargotHuet <margothuet4@gmail.com>
2023-10-31 15:35:08 +0100

    first commit on new branch

--- back/app.js
+++ back/app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 const supabase = createClient('https://bbrfovbvfzeszrjnhsdp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJicmZvdmJ2Znplc3pyam5oc2RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg2NjQwMDAsImV4cCI6MjAxNDI0MDAwMH0.m59kFNiMCInEjaQcC-v32YOJ4JolEwE9dJruivGi5FQ');
 
+
 //cors
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
