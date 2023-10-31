cd6d4b7b97b42acce1ebe64e25be88f450e0caf7
LouisCoutel <louiscoutel75@gmail.com>
2023-10-31 11:06:51 +0100

    suivi tutoriel maintenant tests

--- Ikeada/src/main.jsx
+++ Ikeada/src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { App, loader as rootLoader } from "./routes/App.jsx";
+import ErrorPage from './ErrorPage.jsx';
+import CardList from './routes/CardList.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "CardList/",
+        element: <CardList />,
+      },
+    ],
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
