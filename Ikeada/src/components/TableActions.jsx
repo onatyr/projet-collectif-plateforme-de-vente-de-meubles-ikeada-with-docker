1f3d71e02a1cc4cb7c0c85449ac730c28b1c1b02
LouisCoutel <louiscoutel75@gmail.com>
2023-11-02 16:38:18 +0100

    cleanup and trying auth

--- Ikeada/src/components/TableActions.jsx
+++ Ikeada/src/components/TableActions.jsx
@@ -1,12 +1,12 @@
 import { PropTypes } from "prop-types";
 
-import DeleteButton from "./atoms/DeleteButton";
-import EditButton from "./atoms/EditButton";
-import StachButton from "./atoms/StachButton";
-import PublishButton from "./atoms/PublishButton";
+import DeleteButton from "./DeleteButton";
+import EditButton from "./EditButton";
+import StachButton from "./StachButton";
+import PublishButton from "./PublishButton";
 
 export default function TableActions({ status, available }) {
-  if (status && available) {
+  if (status) {
     return (
       <td direction="horizontal" className="text-center">
         <EditButton />
