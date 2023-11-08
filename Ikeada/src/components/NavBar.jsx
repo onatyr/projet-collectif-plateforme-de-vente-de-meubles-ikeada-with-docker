c23c4829272f8ff40600b84eae85029781cc2a3f
Jezzatator <jeremie.patot@protonmail.ch>
2023-11-08 16:48:21 +0100

    fix habillage bar de recherche

--- Ikeada/src/components/NavBar.jsx
+++ Ikeada/src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 import { Container, Navbar, Nav, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { HouseDoorFill, PersonFill, GearFill } from "react-bootstrap-icons";
 import { Button } from "react-bootstrap";
 import { useState } from "react";
 import BorderBottom from "./atoms/BorderBottom";
@@ -10,7 +9,12 @@ export function NavBar() {
   const [query, setQuery] = useState("");
 
   return (
-    <Navbar fixed="top" sticky="top" expand="lg" className="navbar">
+    <Navbar
+      fixed="top"
+      sticky="top"
+      expand="lg"
+      className="navbar d-flex justify-content-between"
+    >
       <Container fluid className="navbar--container">
         <Navbar.Brand href="#">Ikeada</Navbar.Brand>
         <Nav
@@ -18,32 +22,34 @@ export function NavBar() {
           style={{ maxHeight: "100px" }}
           navbarScroll
         ></Nav>
-        <Nav className="mx-auto d-flex gap-4">
+        <Nav className="mx-auto d-flex flew-row justify-content-around gap-4">
           {/* on peut naviguer vers différentes pages grace aux Routes */}
-          <Link to={`/shop`} className="navbar--text--link">
+          <Link to={`/shop`} className="navbar--text--link p-2">
             Shop
           </Link>
-          <Link to={`/back-office`} className="navbar--text--link">
+          <Link to={`/back-office`} className="navbar--text--link p-2">
             À Propos
           </Link>
-          <Link to={`/contact`} className="navbar--text--link">
+          <Link to={`/contact`} className="navbar--text--link p-2">
             Contact
           </Link>
         </Nav>
-        <Form className="d-flex">
+        <Form className="d-flex justify-content-end navbar--form--wrapper">
           <Form.Control
             type="search"
             placeholder="Un møbl à trouver ?"
-            className="me-2 navbar--search--textfield"
+            className="me-2 navbar--search--textfield p-2"
             aria-label="Un møbl à trouver ?"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
           />
           {/* on insère le contenu du champ ici pour accèder à la page correspondante via une Route */}
-          <Link to={`../shop/search/${query}`}>
-            <Button variant="outline-success" className="navbar--search--btn">
-              Search
-            </Button>
+          <Link
+            to={`../shop/search/${query}`}
+            variant="outline-success"
+            className="btn btn-outline-custom navbar--search--btn p-2"
+          >
+            Search
           </Link>
         </Form>
         <BorderBottom />
