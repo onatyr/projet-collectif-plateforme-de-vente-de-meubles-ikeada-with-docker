ecbe3034a99105b22e26d9fdfa5c717b0913d250
Jezzatator <jeremie.patot@protonmail.ch>
2023-11-08 16:55:50 +0100

    ajout de la ligne toutes les pieces dans la list des categs

--- Ikeada/src/components/Home/CategList.jsx
+++ Ikeada/src/components/Home/CategList.jsx
@@ -3,7 +3,9 @@ import { useParams } from "react-router";
 import { useEffect } from "react";
 import { categsStore } from "../../stores/categStore";
 import { observer } from "mobx-react-lite";
-import ListGroup from "react-bootstrap/ListGroup";
+import { ArrowUpRight } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
+import { ListGroup } from "react-bootstrap";
 
 const CategList = observer(() => {
   const urlParam = useParams();
@@ -16,6 +18,14 @@ const CategList = observer(() => {
 
   return (
     <ListGroup variant="flush" className="list-group p-2 bd-highlight">
+      <ListGroup.Item
+        className="list-group-item d-flex justify-content-between"
+        as={Link} // encodeURI("Salle à manger")
+        to={`../shop`}
+      >
+        <p className="categ--text--list fs-3">Toutes les pièces</p>{" "}
+        <ArrowUpRight />
+      </ListGroup.Item>
       {categs.map((categ) => {
         return <CardRowList key={categ.name} categ={categ} />;
       })}
